718249f28a2046721f1bc26b613883715519276f
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-03-23 01:13:13 +1300

    data verified

--- test/nzcp.js
+++ test/nzcp.js
@@ -53,7 +53,7 @@ async function testNZCPPubIdentity(cir, passURI, isLive, maxLen) {
     const toBeSignedHash = new Uint8Array([...Array.from(out1).slice(1), out2[0], out2[1]])
     const nbfBytes = bitArrayToBuffer(evmRearrangeBits(bufferToBitArray([out2[5], out2[4], out2[3], out2[2]])))
     const expBytes = bitArrayToBuffer(evmRearrangeBits(bufferToBitArray([out2[9], out2[8], out2[7], out2[6]])))
-    const data = new Uint8Array(Array.from(out2).slice(10))
+    const data = bitArrayToBuffer(evmRearrangeBits(bufferToBitArray(Array.from(out2).slice(10).reverse())))
     console.log('nullifierHashPart', nullifierHashPart)
     console.log('toBeSignedHash', toBeSignedHash)
     
